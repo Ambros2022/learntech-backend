4d79736f7a3d9b9e32d4b5eb13cbadf43bad0c34
Ambros tech <109848733+Ambros2022@users.noreply.github.com>
2024-05-25 12:25:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,56 +2,59 @@ const express = require("express");
 const cors = require("cors");
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const _ = require('lodash');
+const path = require('path'); // Import path module
 const app = express();
-const https = require("https");
 const http = require("http");
-const fs = require('fs');
-
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://preprod.keralastudy.com/');
-  // You can also set other CORS headers as needed
+  res.header('Access-Control-Allow-Origin', 'http://preprod.keralastudy.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-
-
 var corsOptions = {
-  origin: ["http://localhost:3000","http://preprod.keralastudy.com/","http://bstudy.codepixelsoft.com","http://localhost:3001","http://preprod.bangalorestudy.com","https://preprod.bangalorestudy.com","https://bangalorestudy.com"]
+  origin: [
+    "http://localhost:3000",
+    "http://preprod.keralastudy.com",
+    "http://bstudy.codepixelsoft.com",
+    "http://localhost:3001",
+    "http://preprod.bangalorestudy.com",
+    "https://preprod.bangalorestudy.com",
+    "https://bangalorestudy.com"
+  ]
 };
 
 app.use(cors(corsOptions));
 
-
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+  createParentPath: true
+}));
 
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// simple route
+// Default route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Learntechww.comww1122" });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get("/api", (req, res) => {
+ res.json({ message: "Welcome to Learntechww.comww1122" });
 });
 
-// creating the http server for development
+// Start the HTTP server
 let portnumber = 5000;
 
 var httpServer = http.createServer(app);
 httpServer.listen(portnumber);
 
 httpServer.on("listening", function () {
-  console.log("ok, server is runningqq: " + portnumber);
+  console.log("ok, server is running on portq: " + portnumber);
 });
-
-
