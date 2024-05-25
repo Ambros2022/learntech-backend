af19aac8d29b2248e9f1eb88a5bf6a52984c1fc6
Ambros tech <109848733+Ambros2022@users.noreply.github.com>
2024-05-25 12:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-// enable files upload
 
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 },
-  createParentPath: true
-}));
 // parse requests of content-type - application/json
 app.use(express.json());
 
@@ -46,19 +41,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Learntechww.comww11" });
+  res.json({ message: "Welcome to Learntechww.comww1122" });
 });
-
-// app.use('/', express.static(__dirname + '/storage'));
-// app.use('/storage', express.static(__dirname + '/storage'));
-
-
-
-
-// require('./app/routes/auth.routes')(app);
-// require('./app/routes/user.routes')(app);
-// require('./app/routes/admin.routes')(app);
-// require('./app/routes/web.routes')(app);
 
 // creating the http server for development
 let portnumber = 5000;
@@ -69,29 +53,5 @@ httpServer.listen(portnumber);
 httpServer.on("listening", function () {
   console.log("ok, server is runningqq: " + portnumber);
 });
-
-
-
-//this is for live server and secure connect +++++ https ++++
-
-// set port, listen for requests
-// const PORT = process.env.APP_PORT || 3000;
-
-// // serve the API with signed certificate on 443 (SSL/HTTPS) port
-// const httpsServer = https.createServer({
-//   key: fs.readFileSync('/etc/letsencrypt/live/api.bangalorestudy.com/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/api.bangalorestudy.com/fullchain.pem'),
-// }, app);
-
-
-
-// httpsServer.listen(3000, () => {
-//     console.log('HTTPS Server running on port 443');
-// });
-/*
-app.listen(PORT, () => {
-  //logger.log('info', '--------------------------------------------------');
-  console.log(`Server is running on port ${PORT}.`);
-});*/
 
 
