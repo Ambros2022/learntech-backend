b04db0d3ba2c610453f5fc94e0638fb888e47583
Ambros tech <109848733+Ambros2022@users.noreply.github.com>
2024-05-25 11:46:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Learntechww.comww" });
+  res.json({ message: "Welcome to Learntechww.comww11" });
 });
 
 app.use('/', express.static(__dirname + '/storage'));
