fa3dd0a84d55719ae5c415f7826eed2d851adaa5
abhishek <abhishekdahiya298@gmail.com>
2024-06-14 10:42:17 +0530

    F

--- app/controllers/website/webapi.controller.js
+++ app/controllers/website/webapi.controller.js
@@ -1457,48 +1457,44 @@ exports.abroadpages = async (req, res) => {
 
 exports.abroadcollegefindone = (req, res) => {
   const slug = req.params.slug;
-  // abroadpages.findByPk(id, 
-  abroadpages.findOne({ where: { slug: slug } },
 
-    {
-      attributes: ['id', 'country_id', 'name', 'slug', 'backgroundimage'],
-      include: [
-        {
-          required: false,
-          association: "country",
-          attributes: ["id", "name"],
-        },
-        {
-          required: false,
-          association: "abroadpagefaqs",
-          attributes: ["id", "questions", "answers"],
-        },
-
-      ],
-    })
+  abroadpages.findOne({
+    where: { slug: slug },
+    include: [
+      {
+        required: false,
+        association: "country",
+        attributes: ["id", "name"],
+      },
+      {
+        required: false,
+        association: "abroadpagefaqs",
+        attributes: ["id", "questions", "answers"],
+      },
+    ],
+  })
     .then((data) => {
       if (data) {
-
-
         res.status(200).send({
           status: 1,
-          message: "successfully retrieved",
+          message: "Successfully retrieved",
           data: data,
         });
       } else {
         res.status(400).send({
           status: 0,
-          message: `Cannot find abroadpages with id=${slug}.`,
+          message: `Cannot find abroadpages with slug=${slug}.`,
         });
       }
     })
     .catch((err) => {
       res.status(500).send({
         status: 0,
-        message: "Error retrieving abroadpages with id=" + slug,
+        message: "Error retrieving abroadpages with slug=" + slug,
       });
     });
 };
+
 
 exports.allentranceexams = async (req, res) => {
   const { page, size, searchtext, searchfrom, stream_id, columnname, orderby, promo_banner_status } = req.query;
