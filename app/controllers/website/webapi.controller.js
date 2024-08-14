90503b0fbdd50b4121e354653f62a2a6389e9f3e
abhishek <abhishekdahiya298@gmail.com>
2024-08-14 15:23:30 +0530

    all states api changed

--- app/controllers/website/webapi.controller.js
+++ app/controllers/website/webapi.controller.js
@@ -119,68 +119,135 @@ exports.allcountries = async (req, res) => {
       });
     });
 };
-
 exports.allstates = async (req, res) => {
   const { page, size, searchtext, searchfrom, country_id, columnname, orderby } = req.query;
 
-  var column = columnname ? columnname : "id";
-  var order = orderby ? orderby : "ASC";
-  var orderconfig = [column, order];
+  let column = columnname || "id";
+  let order = orderby || "ASC";
+  let orderconfig = [column, order];
 
   const myArray = column.split(".");
   if (typeof myArray[1] !== "undefined") {
-    var table = myArray[0];
+    const table = myArray[0];
     column = myArray[1];
     orderconfig = [table, column, order];
   }
+
   let data_array = [];
   let conditionCountryId = country_id ? { country_id: country_id } : null;
-  conditionCountryId ? data_array.push(conditionCountryId) : null;
+  if (conditionCountryId) data_array.push(conditionCountryId);
 
-  var condition = sendsearch.customseacrh(searchtext, searchfrom);
-  condition ? data_array.push(condition) : null;
+  const condition = sendsearch.customseacrh(searchtext, searchfrom);
+  if (condition) data_array.push(condition);
 
   const { limit, offset } = getPagination(page, size);
-  state
-    .findAndCountAll({
-      where: data_array, limit, offset,
-      attributes: [
-        "id",
-        "name",
-        "country_id",
-      ],
 
+  try {
+    const data = await state.findAndCountAll({
+      where: {
+        [db.Sequelize.Op.and]: [
+          ...data_array,
+          {
+            id: {
+              [db.Sequelize.Op.in]: db.sequelize.literal(`(
+                SELECT DISTINCT state_id FROM colleges WHERE state_id IS NOT NULL
+              )`)
+            }
+          }
+        ]
+      },
+      limit,
+      offset,
+      attributes: ["id", "name", "country_id"],
       include: [
         {
           required: false,
           association: "city",
           attributes: ["id", "name"],
-
         },
       ],
-      order: [orderconfig]
-    })
-    .then((data) => {
-      const response = getPagingData(data, page, limit);
-
-      res.status(200).send({
-        status: 1,
-        message: "success",
-        totalItems: response.totalItems,
-        currentPage: response.currentPage,
-        totalPages: response.totalPages,
-        data: response.finaldata,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        status: 0,
-        message:
-          err.message ||
-          "Some error occurred while retrieving states.",
-      });
+      order: [orderconfig],
     });
+
+    const response = getPagingData(data, page, limit);
+
+    res.status(200).send({
+      status: 1,
+      message: "success",
+      totalItems: response.totalItems,
+      currentPage: response.currentPage,
+      totalPages: response.totalPages,
+      data: response.finaldata,
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: 0,
+      message: err.message || "Some error occurred while retrieving states.",
+    });
+  }
 };
+
+// exports.allstates = async (req, res) => {
+//   const { page, size, searchtext, searchfrom, country_id, columnname, orderby } = req.query;
+
+//   var column = columnname ? columnname : "id";
+//   var order = orderby ? orderby : "ASC";
+//   var orderconfig = [column, order];
+
+//   const myArray = column.split(".");
+//   if (typeof myArray[1] !== "undefined") {
+//     var table = myArray[0];
+//     column = myArray[1];
+//     orderconfig = [table, column, order];
+//   }
+//   let data_array = [];
+//   let conditionCountryId = country_id ? { country_id: country_id } : null;
+//   conditionCountryId ? data_array.push(conditionCountryId) : null;
+
+//   var condition = sendsearch.customseacrh(searchtext, searchfrom);
+//   condition ? data_array.push(condition) : null;
+
+//   const { limit, offset } = getPagination(page, size);
+//   state
+//     .findAndCountAll({
+//       where: data_array, limit, offset,
+//       attributes: [
+//         "id",
+//         "name",
+//         "country_id",
+//       ],
+
+//       include: [
+//         {
+//           required: false,
+//           association: "city",
+//           attributes: ["id", "name"],
+
+//         },
+//       ],
+//       order: [orderconfig]
+//     })
+//     .then((data) => {
+//       const response = getPagingData(data, page, limit);
+
+//       res.status(200).send({
+//         status: 1,
+//         message: "success",
+//         totalItems: response.totalItems,
+//         currentPage: response.currentPage,
+//         totalPages: response.totalPages,
+//         data: response.finaldata,
+//       });
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         status: 0,
+//         message:
+//           err.message ||
+//           "Some error occurred while retrieving states.",
+//       });
+//     });
+// };
 
 exports.allstream_exams = async (req, res) => {
   const { page, size, searchtext, searchfrom, columnname, orderby, } = req.query;
