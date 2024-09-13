5ef3dc8ba639c61c2fa60ea387465a5cedb24c0c
amankumar0149 <amankumar0149@gmail.com>
2024-09-13 18:27:23 +0530

    ..

---
+++ app/models/training_team.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+    const training_teams = sequelize.define("training_teams", {
+        name: {
+            type: Sequelize.STRING,
+        },
+        location: {
+            type: Sequelize.STRING
+        },
+        experience : {
+            type: Sequelize.STRING
+        },
+    }
+        ,
+        {
+
+            timestamps: true,
+            underscored: true,
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+
+        }
+
+    );
+
+    return training_teams;
+};
